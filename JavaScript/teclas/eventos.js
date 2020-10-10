1f5d7ab1b35e9e61eba250b7c7fc2e212afd175a
Hector Rendon <flakonet@msn.com>
2020-10-10 10:59:36 -0500

    Dibujando en canvas con flechas del teclado

---
+++ JavaScript/teclas/eventos.js
@@ -0,0 +1,55 @@
+var tecla = {
+    UP:38,
+    DOWN:40,
+    LEFT:37,
+    RIGHT:39
+};
+
+document.addEventListener("keyup",dibujarTeclado);
+
+var cuadrito = document.getElementById("area_de_dibujo");
+var papel = cuadrito.getContext("2d");
+var x = 250;
+var y = 250;
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal, lienzo)
+{
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.lineWidth = 3;
+    lienzo.moveTo(xinicial, yinicial);
+    lienzo.lineTo(xfinal, yfinal);
+    lienzo.stroke();
+    lienzo.closePath();
+}
+
+dibujarLinea("blue", x-1,y-1,x+1,y+1, papel); //Creamos un punto donde iniciar a dibujar
+
+function dibujarTeclado(e)
+{
+    var colorcito = "blue"; //Color del dibujo
+    var movimiento = 5; //Cantidad de pixeles en el movimiento
+
+    switch(e.keyCode)
+    {
+        case tecla.UP:
+            dibujarLinea(colorcito,x,y,x,y-movimiento, papel);
+            y = y-movimiento;
+            break;
+        case tecla.DOWN:
+            dibujarLinea(colorcito,x,y,x,y+movimiento, papel);
+            y = y+movimiento;
+            break;
+        case tecla.LEFT:
+            dibujarLinea(colorcito,x,y,x-movimiento,y, papel);
+            x = x-movimiento;
+            break;
+        case tecla.RIGHT:
+            dibujarLinea(colorcito,x,y,x+movimiento,y, papel);
+            x = x+movimiento;
+            break;
+        default:
+            console.log("Otra tecla");
+            break;
+    }
+}
